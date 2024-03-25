d8b92e0612cef0f14c501fc31449371a203a0cd6
Nicolas Jacquin <36972435+Gortaf@users.noreply.github.com>
2024-03-25 11:46:07 -0400

    Added source

---
+++ src/Kct.jl
@@ -0,0 +1,363 @@
+using Kmers
+using JSON
+using ElasticArrays
+using ProgressMeter
+include("parallel_sort.jl")
+
+export Kct, Kct, load, save, merge
+
+struct KRecord
+    seq::DNA27mer
+    pool_id::UInt32
+end
+
+Base.isless(a::KRecord, b::KRecord) = a.seq < b.seq
+Base.isless(a::DNA27mer, b::KRecord) = a < b.seq
+Base.isless(a::KRecord, b::DNA27mer) = a.seq < b
+Base.:(==)(a::KRecord, b::KRecord) = a.seq == b.seq
+
+const mask_big = 0x00000001 << 27
+isbig(x::UInt32) = x & mask_big != 0
+makebig(x::Integer) = UInt32(x) | mask_big
+idbig(x::UInt32) = x & (~mask_big)
+
+struct Kct{N}
+    table::Vector{KRecord}
+    idx::Vector{UnitRange{Int64}}
+    small::ElasticMatrix{UInt8}
+    big::ElasticMatrix{UInt32}
+end
+
+Kct(N, t=0, s=0, b=0) = Kct{N}(
+    Vector{KRecord}(undef, t),
+    fill(0:-1, 4^11),
+    Matrix{UInt8}(undef, N, s),
+    Matrix{UInt32}(undef, N, b)
+)
+
+function Base.push!(kct::Kct{N}, count::Matrix{UInt32}) where {N}
+    if max(count...) ≤ typemax(UInt8)
+        append!(kct.small, count)
+        (_, id) = UInt32.(size(kct.small))
+    else
+        append!(kct.big, count)
+        (_, id) = UInt32.(size(kct.big))
+        id |= mask_big
+    end
+    return id
+end
+
+function Base.push!(kct::Kct{N}, count::Matrix{UInt8}) where {N}
+    append!(kct.small, count)
+    (_, id) = UInt32.(size(kct.small))
+    return id
+end
+
+function Base.push!(kct::Kct{N}, seq::DNA27mer, count::Matrix{UInt32}) where {N}
+    id = push!(kct, count)
+    push!(kct.table, KRecord(seq, id))
+    return id
+end
+
+function Base.getindex(kct::Kct{N}, i::Integer) where {N}
+    rec = kct.table[i]
+    if mask_big & rec.pool_id != 0
+        tmp_t = convert(Vector{UInt32}, kct.big[:, rec.pool_id & (~mask_big)])
+    else
+        tmp_t = convert(Vector{UInt32}, kct.small[:, rec.pool_id])
+    end
+    return rec.seq, tmp_t
+end
+
+
+function Base.getindex(kct::Kct{N}, idx::AbstractArray{M}) where {N, M<:Integer}
+    return [kct[i] for i in idx]
+end
+
+#= 
+Deletes an element in the table, but does not check if the corresponding count vector
+is no longer being pointed at by any element in the table. This can lead to garbage 
+data left in memory until the whole Kct is freed.
+Do not rely on this to make a Kct smaller. This is just a convenience thing
+for when we need to work with indexes on a Kct.
+=#
+function Base.deleteat!(kct::Kct{N}, idx::Int64) where {N}
+    deleteat!(kct.table, idx)
+end
+
+function Base.deleteat!(kct::Kct{N}, idx::AbstractArray{M}) where {N, M<:Integer}
+    for i in idx
+        deleteat!(kct, i)
+    end
+end
+
+#= 
+deleteat scales very powerly with big kcts, since it requires all elements beyond the
+deleted one to be moved in memory.
+quick_delete instead moves the last element to the position of the elmenent to delete
+and then cuts of the tail of the table, meaning no re-assignation nescessary, which
+means this is in constant time, regardless of kct size (which is pretty nice).
+However, this does NOT preserve order in the table, which means this should
+never be used before the kct is finished merging with others, or if you
+want to be able to search k-mers using dichotomic search.
+=#
+function quick_delete!(kct::Kct{N}, idx::Int64) where {N}
+    kct.table[idx] = kct.table[end]
+    deleteat!(kct.table, length(kct.table))
+end
+
+function quick_delete!(kct::Kct{N}, idx::AbstractArray{M}) where {N, M<:Integer}
+    for i in idx
+        quick_delete!(kct, i)
+    end
+end
+
+function Base.iterate(kct::Kct, state=1)
+    state > length(kct) && return nothing
+    return (kct[state], state+1)
+end
+
+Base.length(kct::Kct) = length(kct.table)
+
+# Base.getindex(kct::Kct{N,T}, v::Vector{U}) where {N,T,U<:Integer} = [kct[i] for i in v]
+
+# function Base.getindex(kct::Kct{N, T}, key::DNA27mer{K})::Tuple{DNA27mer{K}, NTuple{N, UInt32}} where {N, T}
+#     idx_key = (key.data[1] >> 40) + 1
+#     r = kct.idx[idx_key]
+#     t = @view kct.table[r]
+#     i = searchsortedfirst(t, key)
+#     if i > length(t) || t[i].seq != key
+#         return (DNA27mer{K}("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"), NTuple{N, UInt32}(zeros(UInt32, N)))
+#     end
+#     first(kct.pool[t[i].pool_id]) == typemax(T) && return kct.next[key]
+#     return (t[i].seq, convert(NTuple{N, UInt32}, kct.pool[t[i].pool_id]))
+# end
+
+# Base.getindex(kct::Kct, key::String) = kct[DNA27mer{K}(key)]
+
+function Base.show(io::IO, t::Tuple{DNA27mer, NTuple{N, UInt32}}) where {N}
+    print(io, t[1], ": ", join(string.(t[2]), ", "))
+
+end
+
+function Base.sort!(kct::Kct)
+    psort!(kct.table)
+end
+
+function computeIndex!(kct::Kct)
+    start = 1
+    last_key = 0x0000000000000000
+    for i=1:length(kct.table)
+        key = kct.table[i].seq.data[1] >> 40
+        if key > last_key
+            kct.idx[last_key+1] = start:i
+            start = i
+            last_key = key
+        end
+    end
+end
+
+function _dedup(kct, nb)
+    o = psortperm(nb)
+    last = 0
+    id = 0
+    buf = Matrix{UInt32}(undef, 1, 1)
+    for i in o
+        if nb[i] != last
+            buf[1, 1] = nb[i]
+            id = push!(kct, buf)
+        end
+        kct.table[i] = KRecord(kct.table[i].seq, id)
+        last = nb[i]
+    end
+
+    return kct
+end
+
+"""
+    Kct(fn::String)
+
+Parse a Jellyfish K-mer count table into a sorted array of DNA27mer. No
+verification is made on the header, use with caution. 
+"""
+function Kct(fn::String; k::Int=31)
+    f = open(fn, "r")
+    offset = parse(Int, readuntil(f, "{"))
+    json_start = position(f)
+    seek(f, position(f) - 1) # Need to grab the {
+    header = JSON.parse(f)
+    println(header)
+    seek(f, offset + json_start - 1) 
+
+    nb = Vector{UInt32}()
+    kct = Kct(1)
+
+    while(!eof(f))
+        tmp_seq = DNA27mer{k}((read(f, UInt64),))
+        tmp_nb = read(f, UInt32)
+        # println(tmp_seq, " : ", tmp_nb)
+        # return
+        push!(kct.table, KRecord(tmp_seq, UInt32(0)))
+        push!(nb, tmp_nb)
+    end
+
+    _dedup(kct, nb)
+    
+    sort!(kct)
+
+    # computeIndex!(kct)
+
+    return kct
+end
+
+# uses the dump instead of the binary. tedious and inelegant, but a good backup_kct
+# in case reading the binary fails
+function backup_kct(fn::String; k::Int64=31)
+    io = IOBuffer()
+    cmd = pipeline(`/soft/bioinfo/linux_RH7/python-2.7.6/bin/jellyfish dump $fn`; stdout=io, stderr=devnull)
+    run(cmd)
+    data = IOBuffer(String(take!(io)))  # OUCH!!!
+    nb = Vector{UInt32}()
+    kct = Kct(1)
+    prog = ProgressUnknown()
+    while(!eof(data))
+        tmp_nb = parse(UInt32, readline(data)[2:end])
+        tmp_seq = DNA27mer(readline(data))
+        push!(kct.table, KRecord(tmp_seq, UInt32(0)))
+        push!(nb, tmp_nb)
+        next!(prog)
+    end
+
+    _dedup(kct, nb)
+    
+    sort!(kct)
+
+    # computeIndex!(kct)
+
+    return kct
+end
+
+function Base.write(s::IO, kct::Kct{N}) where {N}
+    write(s, N)
+    write(s, length(kct.table))
+    write(s, size(kct.small)[2])
+    write(s, size(kct.big)[2])
+    write(s, kct.table)
+    write(s, kct.small)
+    write(s, kct.big)
+end
+
+function Base.read(s::IO, ::Type{Kct})
+    N = read(s, Int)
+    l = read(s, Int)
+    ls = read(s, Int)
+    lb = read(s, Int)
+    kct = Kct(N, l, ls, lb)
+    read!(s, kct.table)
+    read!(s, kct.small)
+    read!(s, kct.big)
+    computeIndex!(kct)
+    return kct
+end
+
+function Base.findfirst(kct::Kct{N}, key::DNA27mer) where {N}
+    idx_key = (key.data[1] >> 40) + 1
+    r = kct.idx[idx_key]
+    t = @view kct.table[r]
+    i = searchsortedfirst(t, key)
+    if i > length(t) || t[i].seq != key
+        return 0
+    end
+    return r[1] + i-1
+end
+
+"""
+    save(kct::Kct, fn::String)
+
+Saves a K-mer count table into a binary format file.
+"""
+function save(kct::Kct{N}, fn::String) where {N}
+    f = open(fn, "w")
+    write(f, kct)
+    close(f)
+end
+
+"""
+    load(fn::String)::Kct
+
+Loads a K-mer count table from a binary format file.
+"""
+function load(fn::String)::Kct
+    f = open(fn, "r")
+    return read(f, Kct)
+end
+
+# function _getpool(kct::Kct, pool_id::UInt32)::Union{ElasticVector{UInt8}, ElasticVector{UInt32}}
+#     isbig(pool_id) && return kct.big[:,idbig(pool_id)]
+#     return kct.small[:,pool_id]
+# end
+
+function _mergededup(kct::Kct, a::Kct{M}, b::Kct{N}, p) where {M,N}
+    o = psortperm(p)
+    last::Tuple{UInt32, UInt32} = (0, 0)
+    id::UInt32 = 0
+    for i in o
+        if p[i] != last
+            ia, ib = p[i]
+            if isbig(ia) || isbig(ib)
+                bbuf = zeros(UInt32, M+N, 1)
+                if isbig(ia)
+                    bbuf[1:M,1] = a.big[:,idbig(ia)]
+                elseif ia != 0
+                    bbuf[1:M,1] = a.small[:,ia]
+                end
+                if isbig(ib)
+                    bbuf[(M+1):(M+N),1] = b.big[:,idbig(ib)]
+                elseif ib != 0
+                    bbuf[(M+1):(M+N),1] = b.small[:,ib]
+                end
+                id = push!(kct, bbuf)
+            else
+                buf = zeros(UInt8, M+N, 1)
+                if ia != 0
+                    buf[1:M,1] = a.small[:,ia]
+                end
+                if ib != 0
+                    buf[(M+1):(M+N),1] = b.small[:,ib]
+                end
+                id = push!(kct, buf)
+            end
+        end
+        kct.table[i] = KRecord(kct.table[i].seq, id)
+        last = p[i]
+    end
+end
+
+function Base.merge(a::Kct{M}, b::Kct{N}) where {M, N}
+    i = 1
+    j = 1
+
+    kct = Kct(M+N)
+    p = Vector{Tuple{UInt32, UInt32}}()
+
+    while i ≤ length(a) || j ≤ length(b)
+        if j > length(b) || (i ≤ length(a) && a.table[i].seq < b.table[j].seq)
+            push!(p, (a.table[i].pool_id, 0))
+            push!(kct.table, KRecord(a.table[i].seq, 0))
+            i += 1
+        elseif i > length(a) || (j ≤ length(b) && b.table[j].seq < a.table[i].seq)
+            push!(p, (0, b.table[j].pool_id))
+            push!(kct.table, KRecord(b.table[j].seq, 0))
+            j += 1
+        else
+            push!(p, (a.table[i].pool_id, b.table[j].pool_id))
+            push!(kct.table, KRecord(a.table[i].seq, 0))
+            i += 1
+            j += 1
+        end
+    end
+
+    _mergededup(kct, a, b, p)
+    # return kct, p
+    return kct
+end
---
+++ src/Modron.jl
@@ -0,0 +1,304 @@
+import Pkg
+Pkg.activate("/u/jacquinn/Marut/Modron")
+
+using ProgressMeter
+# using Combinatorics
+using Kmers
+using ArgParse
+
+include("Kct.jl")
+
+abstract type Abstract_Node end
+
+mutable struct Tree_Node <: Abstract_Node
+    kct_index::UInt32
+    to::Vector{Tree_Node}
+    sample::Int
+end
+get_seq(node::Abstract_Node, kct::Kct{N}) where {N} = String(kct[node.kct_index][1])
+
+mutable struct Tree_Root <: Abstract_Node
+    seed::String
+    to::Vector{Abstract_Node}
+    sample::Int
+end
+get_seq(node::Tree_Root, kct::Kct{N}) where {N} = node.seed
+
+const nucleotides = ('A', 'T', 'G', 'C')
+
+function parse_commands()
+    arg_settings = ArgParseSettings()
+    @add_arg_table arg_settings begin
+        "walk"
+            help = "Signals k-mer walking mode."
+            action = :command
+        "prepare"
+            help = "Signals kct-preparation mode."
+            action = :command
+    end
+
+    @add_arg_table arg_settings["walk"] begin
+        "--seed", "-s"
+            help = "The seed to extend by k-mer walking."
+            arg_type = String
+            required = true
+        "--library", "-l"
+            help = "The KCT library to use for k-mer walking."
+            arg_type = String
+            required = true
+        "--output", "-o"
+            help = "Output file. Will be in fasta64format."
+            arg_type = String
+            required = true
+        "--max_recursion", "-r"
+            help = "How much the seed can be extended by k-mer walking."
+            arg_type = Int
+            default = 500
+        "--min_count", "-c"
+            help = "The minimum count for a k-mer to be admissible. Should be >0."
+            arg_type = Int
+            default = 1
+    end
+
+    @add_arg_table arg_settings["prepare"] begin
+        "--data", "-d"
+            help = "Folder with jellyfish files to build a KCT from."
+            arg_type = String
+            required = true
+        "--output", "-o"
+            help = "The output kct file. Will be in binary format."
+            arg_type = String
+            required = true    
+    end   
+
+    return parse_args(arg_settings)
+end
+
+
+function build_kct(jf_folder::String, output::String)
+    jf_files = readdir(jf_folder; join = true)
+    if length(jf_files) == 1
+        save(backup_kct(jf_files[1]), output)
+        return
+    end
+
+    # Init stuff
+    prog = Progress(length(jf_files), "Parsing Jellyfish Files (binary tree version)"); update!(prog)
+
+    # Stacks previously loaded Kct
+    tree_stack = Array{Kct, 1}()
+
+    # Using a stack to emulate a binary tree merge. Adding a new leaf at every loop
+    for file in jf_files
+        push!(tree_stack, backup_kct(file))
+        
+        # Then checking if that leaf can be merged with another leaf at the same level.
+        # Keep merging up until no matching leaf at that level.
+        while length(tree_stack) >= 2 && typeof(tree_stack[end]) == typeof(tree_stack[end-1])
+            update!(prog, showvalues = [("Tree size on RAM: ", Base.format_bytes(Base.summarysize(tree_stack))),
+                                        ("Merging", "["*join([typeof(k) for k in tree_stack[1:end-1]], " | ")*" ← $(typeof(tree_stack[end]))]")])
+            last = pop!(tree_stack)
+            push!(tree_stack, (merge(pop!(tree_stack), last)))
+        end
+
+        # Updating progress bar
+        next!(prog; showvalues = [("Tree size on RAM: ", Base.format_bytes(Base.summarysize(tree_stack)))])
+    end
+    finish!(prog)
+    # Extract the root node
+    final_node = popfirst!(tree_stack)
+    # JuBox.save(final_node, save_path*"leucegene_$(typeof(final_node))_samples_bt_backup.kct")
+    
+    # Cleanup any potential leftover leaf. Merges them linearly regardless of level.
+    prog = Progress(length(tree_stack)-1, "Remaining merges to cleanup outstanding leaves"); update!(prog)
+    for i in 1:length(tree_stack)-1
+        pushfirst!(tree_stack, merge(popfirst!(tree_stack), popfirst!(tree_stack)))
+        next!(prog)
+    end
+    finish!(prog)
+    # JuBox.save(tree_stack[end], output)
+
+    # Merging root node and cleanup node, which is the entirely built kct, in order of samples.
+    final_node = merge(final_node, pop!(tree_stack))
+
+    # Saving that kct
+    save(final_node, output)
+
+end
+
+function kct_walk(seed::String, library::String, output::String; max_recursion::Int=500, min_count::Int=2)
+    kct = load(library)
+    if length(kct[1][1]) > length(seed)
+        println("Your seed is smaller than provided k-mers. Searching for extended seeds...")
+        seeds = get_extended_seeds(seed, kct)
+    else
+        seeds = [seed]
+    end
+    fasta = ""
+    for seed in seeds
+        forward_root, reverse_root = build_walk_tree(seed, kct, max_recursion, min_count)
+        forward_sequences = recursive_extract(forward_root.to, Vector{String}(repeat([seed], length(forward_root.to))), Dict{String, Vector{Int}}(), kct)
+        reverse_sequences = recursive_extract(reverse_root.to, Vector{String}(repeat([seed], length(reverse_root.to))), Dict{String, Vector{Int}}(), kct, reverse = true)
+
+        to_fasta(seqs, dir) = join([length(seq)>length(seed) ? ">$dir samples: $samples\n$seq\n" : "" for (seq, samples) in seqs])
+        fasta *= to_fasta(forward_sequences, "forward")*to_fasta(reverse_sequences, "reverse")
+    end
+    open(output, "w") do file_handle
+        write(file_handle, fasta)
+    end
+end
+
+function build_walk_tree(seed::String, kct::Kct{N}, max_recursion::Int, min_count::Int) where {N}
+    
+    forward_root = Tree_Root(seed, Vector{Abstract_Node}(), 0)
+    reverse_root = Tree_Root(seed, Vector{Abstract_Node}(), 0)
+    forward_leaves = Vector{Abstract_Node}([forward_root])
+    reverse_leaves = Vector{Abstract_Node}([reverse_root])
+    max_forward_leaves = 0
+    max_reverse_leaves = 0
+    prog = Progress(max_recursion)
+    
+    for i in 1:max_recursion
+        new_forward_leaves = Vector{Abstract_Node}()
+        new_reverse_leaves = Vector{Abstract_Node}()
+        for leaf in forward_leaves
+            seq = get_seq(leaf, kct)
+            alternates = [seq[2:end]*n for n in nucleotides]
+            for alternate in alternates
+                kct_index = findfirst(kct, DNAKmer(alternate))
+                if kct_index != 0
+                    if leaf.sample == 0
+                        for (s, c) in enumerate(kct[kct_index][2])
+                            if c >= min_count
+                                new_node = Tree_Node(kct_index, Vector{Tree_Node}(), s)
+                                push!(leaf.to, new_node)
+                                push!(new_forward_leaves, new_node)
+                            end
+                        end
+                    else
+                        if kct[kct_index][2][leaf.sample] >= min_count
+                            new_node = Tree_Node(kct_index, Vector{Tree_Node}(), leaf.sample)
+                            push!(leaf.to, new_node)
+                            push!(new_forward_leaves, new_node)
+                        end
+                    end
+                end
+            end
+        end
+        for leaf in reverse_leaves # This is disgusting, needs a rewrite
+            seq = get_seq(leaf, kct)
+            alternates = [n*seq[1:end-1] for n in nucleotides]
+            for alternate in alternates
+                kct_index = findfirst(kct, DNAKmer(alternate))
+                if kct_index != 0
+                    if leaf.sample == 0
+                        for (s, c) in enumerate(kct[kct_index][2])
+                            if c >= min_count
+                                new_node = Tree_Node(kct_index, Vector{Tree_Node}(), s)
+                                push!(leaf.to, new_node)
+                                push!(new_reverse_leaves, new_node)
+                            end
+                        end
+                    else
+                        if kct[kct_index][2][leaf.sample] >= min_count
+                            new_node = Tree_Node(kct_index, Vector{Tree_Node}(), leaf.sample)
+                            push!(leaf.to, new_node)
+                            push!(new_reverse_leaves, new_node)
+                        end
+                    end
+                end
+            end
+        end
+        if isempty(new_forward_leaves) && isempty(new_reverse_leaves) break end
+        forward_leaves = new_forward_leaves
+        reverse_leaves = new_reverse_leaves
+        max_forward_leaves = maximum((length(forward_leaves), max_forward_leaves))
+        max_reverse_leaves = maximum((length(reverse_leaves), max_reverse_leaves))
+        next!(prog, showvalues = [("Current forward leaves", length(forward_leaves)),
+                                  ("Current reverse leaves", length(reverse_leaves)),
+                                  ("Max forward leaves", max_forward_leaves),
+                                  ("Max reverse leaves", max_reverse_leaves),
+                                  ("Forward tree memory", Base.format_bytes(Base.summarysize(forward_root))),
+                                  ("Reverse tree memory", Base.format_bytes(Base.summarysize(reverse_root)))])
+    end
+    finish!(prog, showvalues = [("Max forward leaves", max_forward_leaves),
+                                ("Max reverse leaves", max_reverse_leaves)])
+    return forward_root, reverse_root
+end
+
+function recursive_extract(current_leaves::Vector{Abstract_Node}, sequences::Vector{String}, finished::Dict{String, Vector{Int}}, kct::Kct{N}; reverse::Bool=false) where {N}
+    next_leaves = Vector{Abstract_Node}()
+    next_sequences = Vector{String}()
+    # println(all([isempty(leaf.to) for leaf in current_leaves]))
+    
+    for (i, (leaf, sequence)) in enumerate(zip(current_leaves, sequences))
+        # println(leaf)
+        if !reverse
+            to_add = get_seq(leaf, kct)[end]
+            sequences[i] = sequence*to_add
+        else
+            to_add = get_seq(leaf, kct)[1]
+            sequences[i] = to_add*sequence
+        end
+        if isempty(leaf.to)
+            if !haskey(finished, sequences[i])
+                finished[sequences[i]] = Vector{Int}()
+            end
+            push!(finished[sequences[i]], leaf.sample)
+        end
+        # println(leaf.to)
+        for next in leaf.to
+            push!(next_leaves, next)
+            push!(next_sequences, sequences[i])
+        end
+    end
+    if all([isempty(leaf.to) for leaf in current_leaves]) return finished end
+    return recursive_extract(next_leaves, next_sequences, finished, kct, reverse=reverse)
+end
+
+function get_extended_seeds(seed::String, kct::Kct{N}) where {N}
+    prog = Progress(length(kct.table), desc="Searching for extended seeds...")
+    seeds = Vector{String}()
+    for k in kct.table
+        if occursin(seed, String(k.seq))
+            push!(seeds, k.seq)
+        end
+        next!(prog, showvalues=[("Extended seeds found", length(seeds))])
+    end
+    return seeds
+end
+
+# function prepare_leaves!(forward_root::Tree_Root, reverse_root::Tree_Root, padding::Int)
+#     forward_leaves = Vector{Abstract_Node}([forward_root])
+#     reverse_leaves = Vector{Abstract_Node}([reverse_root])
+#     for i in 1:padding
+#         new_forward_leaves =  Vector{Abstract_Node}()
+#         new_reverse_leaves = Vector{Abstract_Node}()
+#         for (f_leaf, r_leaf) in zip(forward_leaves, reverse_leaves)
+#             for n in nucleotides
+#                 new_f_leaf = Tree_Root(f_leaf.seed*n, Vector{Abstract_Node}(), 0)
+#                 push!(f_leaf.to, new_f_leaf)
+#                 push!(new_forward_leaves, new_f_leaf)
+#                 new_r_leaf = Tree_Root(n*r_leaf.seed, Vector{Abstract_Node}(), 0)
+#                 push!(r_leaf.to, new_r_leaf)
+#                 push!(new_reverse_leaves, new_r_leaf)
+#             end
+#         end
+#         forward_leaves = new_forward_leaves
+#         reverse_leaves = new_reverse_leaves
+#     end
+#     return forward_leaves, reverse_leaves
+# end
+
+function main() 
+parsed_args = parse_commands()
+    if parsed_args["%COMMAND%"] == "walk"
+        sub = parsed_args["walk"]
+        kct_walk(sub["seed"], sub["library"], sub["output"], max_recursion = sub["max_recursion"], min_count = sub["min_count"])
+    elseif parsed_args["%COMMAND%"] == "prepare"
+        sub = parsed_args["prepare"]
+        build_kct(sub["data"], sub["output"])
+    end
+end
+
+main()
